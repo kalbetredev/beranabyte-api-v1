20cb287f97a79f2a4d0b29cc5ed16e83d83118b3
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-07-04 18:13:55 +0300

    validation for unpublished blogs

--- src/api/v1/validation/BlogValidation.ts
+++ src/api/v1/validation/BlogValidation.ts
@@ -4,6 +4,7 @@ const BlogSchema = Joi.object({
   title: Joi.string().required().min(5),
   category: Joi.string().required().min(3),
   isFeatured: Joi.boolean(),
+  isPublished: Joi.boolean().required(),
   publishedAt: Joi.date().required(),
   summary: Joi.string().required(),
   imageUrl: Joi.string().required(),
@@ -11,6 +12,22 @@ const BlogSchema = Joi.object({
   mdx: Joi.string().required().min(500),
 });
 
+const unPublishedBlogSchema = Joi.object({
+  title: Joi.string().required().min(5),
+  category: Joi.string().required().min(3),
+  isFeatured: Joi.boolean(),
+  isPublished: Joi.boolean().required(),
+  publishedAt: Joi.date(),
+  summary: Joi.string(),
+  imageUrl: Joi.string(),
+  viewCount: Joi.number().positive(),
+  mdx: Joi.string(),
+});
+
 export const validateBlog = (blog: any) => {
   return BlogSchema.validate(blog);
+};
+
+export const validateUnpublishedBlog = (blog: any) => {
+  return unPublishedBlogSchema.validate(blog);
 };
