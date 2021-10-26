0bec0e8fa31ec9bb146539bc2e3332dfd32e34fa
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-10-26 14:52:14 +0300

    rename text attribute to content for semantics

--- src/api/v1/validation/CommentValidation.ts
+++ src/api/v1/validation/CommentValidation.ts
@@ -3,7 +3,7 @@ import Joi from "joi";
 const CommentSchema = Joi.object({
   blogId: Joi.string().required(),
   authorId: Joi.string().required(),
-  text: Joi.string().required().min(2).max(500),
+  content: Joi.string().required().min(2).max(500),
   date: Joi.date().required(),
 });
 
