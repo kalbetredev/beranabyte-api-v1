87a2a04d0bcf6ccb5a8304ba79f78cb041e0a0e9
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-10-26 14:59:15 +0300

    remove username attribute

--- src/api/v1/models/User.ts
+++ src/api/v1/models/User.ts
@@ -3,14 +3,12 @@ import { requiredStringSchema } from "./CommonSchemas";
 import { ADMIN_ROLE, NORMAL_ROLE, WRITER_ROLE } from "./UserRoles";
 
 export interface User extends Document {
-  username: string;
   email: string;
   hash: string;
   role: string;
 }
 
 const UserSchema = new Schema<User>({
-  username: requiredStringSchema,
   email: requiredStringSchema,
   hash: requiredStringSchema,
   role: {
