4ae7abd3abda493b191f24aed009f9d7e926fb7b
Kalkidan Betre <kalbetre.dev@outlook.com>
2021-10-26 15:02:04 +0300

    update content schema with the renamed attribute

--- src/api/v1/models/Comment.ts
+++ src/api/v1/models/Comment.ts
@@ -4,7 +4,7 @@ import { requiredStringSchema } from "./CommonSchemas";
 const CommentSchema = new Schema({
   blogId: requiredStringSchema,
   authorId: requiredStringSchema,
-  text: requiredStringSchema,
+  content: requiredStringSchema,
   date: { type: Date, required: true },
 });
 
